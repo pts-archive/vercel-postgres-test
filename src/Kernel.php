a40df0c8c7518ba989abdb285c235538723b897b
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-05-15 08:39:47 +0900

    Add vercel-php configuration

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,22 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        if (isset($_ENV['VERCEL']) || \strtolower($this->environment) === 'prod') {
+            return '/tmp/cache/' . $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir(): string
+    {
+        if (isset($_ENV['VERCEL']) || \strtolower($this->environment) === 'prod') {
+            return '/tmp/log/' . $this->environment;
+        }
+
+        return parent::getLogDir();
+    }
 }
